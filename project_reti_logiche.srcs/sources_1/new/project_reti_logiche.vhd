0d5c9ec1f04e7c75f4a8b25bb8b29ac28065c63f
MarioCela <mario.cela@mail.polimi.it>
2022-10-06 20:54:55 +0200

    Still structure changes
    FSA working, but outputs are wrong

--- project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -1,8 +1,7 @@
-
 -- Politecnico di Milano    AA2021/22
 -- PROVA FINALE     |       Reti Logiche
 -- Alessandro Cogollo 10571078
--- Mario Cela         10XXXXXX
+-- Mario Cela         10685242
 
 ---------------------------------------
 -- + ENTITY
@@ -29,32 +28,53 @@ entity project_reti_logiche is
 end project_reti_logiche;
 
 architecture BEHAVIORAL of project_reti_logiche is
-   
-   type state is (S_WAIT, S_FIRST_WORD, S_COMPARISON, S_READ_WORD, C00, C01, C10, C11, S_SAVE_WORD, S_DONE);
-   signal state_curr, state_next : state;
-   constant READ_BOTTOM     : std_logic_vector (15 downto 0) := "0000000000000000";
-   constant WRITE_BOTTOM    : std_logic_vector (15 downto 0) := "0000001111101000";
-   
-   signal quantity : std_logic_vector (7 downto 0);
-   signal global_counter : INTEGER range 0 to 16385 := 0;
-   signal local_counter : INTEGER range 0 to 8 := 0;
-   signal word_to_process : std_logic_vector (7 downto 0);
-   signal word_to_save : std_logic_vector (7 downto 0);
-   signal temp_value : std_logic;
-            
-   begin
-       process(i_clk, i_rst)
-       begin
-            if (i_rst='1') then
-                state_curr <= S_WAIT;
-            elsif rising_edge(i_clk) then
-                state_curr <= state_next;
-            end if;
-       end process; 
-   
-   process (state_curr, i_start)
-   begin
-       case state_curr is
+    component convolutor is
+        Port (
+            i_clk   : in std_logic;
+            i_rst   : in std_logic;
+            i_data  : in std_logic;
+            i_done  : in std_logic;
+            e_conv  : in std_logic;
+            o_p12k   : out std_logic_vector(1 downto 0)
+        );
+    end component;
+
+    type state is (S_WAIT, S_FIRST_WORD, S_COMPARISON, S_READ_WORD, S_CONV, S_SAVE_P1P2, S_SAVE_WORD1, S_SAVE_WORD_2, S_DONE);
+    signal state_curr, state_next : state;
+    signal i_FSMdata, i_FSMdone, i_FSMconv : std_logic;
+    signal p1p2 : std_logic_vector(1 downto 0) := "00";
+    constant READ_BOTTOM     : std_logic_vector (15 downto 0) := "0000000000000000";
+    constant WRITE_BOTTOM    : std_logic_vector (15 downto 0) := "0000001111101000";
+
+    signal quantity : std_logic_vector (7 downto 0);
+    signal global_counter : INTEGER range 0 to 16385 := 0;
+    signal local_counter : INTEGER range 0 to 8 := 0;
+    signal word_to_process : std_logic_vector (7 downto 0);
+    signal word_to_save : std_logic_vector (15 downto 0) := (others => '0');
+    signal temp_value : std_logic;
+
+begin
+    CONVOLUTOR12: convolutor port map(
+            i_clk => i_clk,
+            i_rst => i_rst,
+            i_data => i_FSMdata,
+            i_done => i_FSMdone,
+            e_conv => i_FSMconv,
+            o_p12k => p1p2
+        );
+
+    process(i_clk, i_rst)
+    begin
+        if (i_rst='1') then
+            state_curr <= S_WAIT;
+        elsif rising_edge(i_clk) then
+            state_curr <= state_next;
+        end if;
+    end process;
+
+    process (state_curr, i_start)
+    begin
+        case state_curr is
             when S_WAIT =>
                 o_address <= READ_BOTTOM;
                 o_en <= '1';
@@ -67,6 +87,7 @@ architecture BEHAVIORAL of project_reti_logiche is
             when S_FIRST_WORD =>
                 -- reading number of words to elaborate
                 quantity <= i_data;
+                o_en <= '0';
                 global_counter <= 0;
                 o_done <= '0';
                 state_next <= S_COMPARISON;
@@ -78,93 +99,123 @@ architecture BEHAVIORAL of project_reti_logiche is
                     global_counter <= global_counter + 1;
                     local_counter <= 0;
                     -- o_address <= "0000000000000010";
-                    o_address <= std_logic_vector(to_unsigned(global_counter, 16));
+                    o_address <= std_logic_vector(to_unsigned(global_counter, 16)) + "1";
+                    o_en <= '1';
+                    o_we <= '0';
                     state_next <= S_READ_WORD;
                 end if;
             when S_READ_WORD =>
                 -- enable counter and memory read
                 word_to_process <= i_data;
-                state_next <= C00;
-            when C00 =>
-                temp_value <= std_logic(word_to_process(0));
-                word_to_process(7 downto 0) <= word_to_process(6 downto 0) & "0";
+                o_en <= '0';
+                state_next <= S_CONV;
+            when S_CONV =>
+                -- convoluting
+                temp_value <= std_logic(word_to_process(7));
+                word_to_process <= word_to_process(6 downto 0) & "0";
                 local_counter <= local_counter + 1;
-                if (local_counter = 4 or local_counter = 8) then
-                    state_next <= S_SAVE_WORD;
+                if (local_counter = 9) then
+                    state_next <= S_SAVE_WORD1;
                 else
-                    if (temp_value = '1') then
-                        word_to_save(7 downto 0) <= word_to_save(5 downto 0) & "11";
-                        -- state_next <= C10;
-                        state_next <= S_WAIT;
-                    else
-                        word_to_save(7 downto 0) <= word_to_save(5 downto 0) & "00";
-                        -- state_next <= C00;
-                        state_next <= C00;
-                    end if;
+                    i_FSMconv <= '1';
+                    i_FSMdata <= temp_value;
+                    state_next <= S_SAVE_P1P2;
                 end if;
-            when C01 =>
-                temp_value <= word_to_process(0);
-                word_to_process(7 downto 0) <= word_to_process(6 downto 0) & "0";
-                local_counter <= local_counter + 1;
-                 if (local_counter = 4 or local_counter = 8) then
-                    state_next <= S_SAVE_WORD;
-                else
-                    if (temp_value = '1') then
-                        word_to_save(7 downto 0) <= word_to_save(5 downto 0) & "00";
-                        state_next <= C10;
-                    else
-                        word_to_save(7 downto 0) <= word_to_save(5 downto 0) & "11";
-                        state_next <= C00;
-                    end if;
-                end if;
-            when C10 =>
-                temp_value <= word_to_process(0);
-                word_to_process(7 downto 0) <= word_to_process(6 downto 0) & "0";
-                local_counter <= local_counter + 1;
-                if (local_counter = 4 or local_counter = 8) then
-                    state_next <= S_SAVE_WORD;
-                else
-                    if (temp_value = '1') then
-                        word_to_save(7 downto 0) <= word_to_save(5 downto 0) & "10";
-                        state_next <= C11;
-                    else
-                        word_to_save(7 downto 0) <= word_to_save(5 downto 0) & "01";
-                        state_next <= C01;
-                    end if;
-                end if;
-            when C11 =>
-                temp_value <= word_to_process(0);
-                word_to_process(7 downto 0) <= word_to_process(6 downto 0) & "0";
-                local_counter <= local_counter + 1;
-                if (local_counter = 4 or local_counter = 8) then
-                    state_next <= S_SAVE_WORD;
-                else
-                    if (temp_value = '1') then
-                        word_to_save(7 downto 0) <= word_to_save(5 downto 0) & "01";
-                        state_next <= C11;
-                    else
-                        word_to_save(7 downto 0) <= word_to_save(5 downto 0) & "10";
-                        state_next <= C01;
-                    end if;
-                end if;
-            when S_SAVE_WORD =>
-                -- saving word in RAM
+            when S_SAVE_P1P2 =>
+                i_FSMconv <= '0';
+                word_to_save <= word_to_save(13 downto 0) & p1p2;
+                state_next <= S_CONV;
+            when S_SAVE_WORD1 =>
+                -- saving word1 in RAM
                 o_en <= '1';
                 o_we <= '1';
-                if (local_counter = 4) then
-                    o_address <= WRITE_BOTTOM + ("00000000" & std_logic_vector(to_unsigned(global_counter, 7))) - '1';
-                    -- o_address <= WRITE_BOTTOM + wordselaborated - "0000000000000001";
-                    o_data <= word_to_save;
-                    state_next <= C00;
-                elsif (local_counter = 8) then
-                   o_address <= WRITE_BOTTOM + ("00000000" & std_logic_vector(to_unsigned(global_counter, 7)));
-                    -- o_address <= WRITE_BOTTOM + wordselaborated - "0000000000000001";
-                    o_data <= word_to_save;
-                    state_next <= S_COMPARISON;
-                end if;
+                o_address <= WRITE_BOTTOM + ("00000000" & std_logic_vector(to_unsigned(global_counter, 8))) - '1';
+                o_data <= word_to_save(15 downto 8);
+                state_next <= S_SAVE_WORD_2;                
+            when S_SAVE_WORD_2 =>
+                -- saving word2 in RAM
+                o_address <= WRITE_BOTTOM + ("00000000" & std_logic_vector(to_unsigned(global_counter, 7)));
+                o_data <= word_to_save(7 downto 0);
+                state_next <= S_COMPARISON;
             when S_DONE =>
                 o_done <= '1';
                 state_next <= S_WAIT;
         end case;
-   end process;  
+    end process;
+end BEHAVIORAL;
+
+
+------------------------------------------
+------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+use IEEE.STD_LOGIC_ARITH.all;
+
+entity convolutor is
+    port (
+        i_clk : in std_logic;
+        i_rst : in std_logic;
+        i_data : in std_logic;
+        i_done : in std_logic;
+        e_conv : in std_logic;
+        o_p12k	: out std_logic_vector(1 downto 0)
+    );
+end entity;
+
+architecture BEHAVIORAL of convolutor is
+
+    type state is (C00,C01,C10,C11);
+    signal state_curr, state_next: state;
+
+begin
+    process(i_clk, i_rst, e_conv, i_done)
+    begin
+        if ((i_rst = '1') or (i_done = '1')) then
+            state_curr <= C00;
+        elsif ((rising_edge(i_clk)) and (e_conv = '1')) then
+            state_curr <= state_next;
+        end if;
+    end process;
+
+    process(state_curr, i_data)
+    begin
+
+        case state_curr is
+            when C00 =>
+                if (i_data = '1') then
+                    o_p12k <= "11";
+                    state_next <= C10;
+                else
+                    o_p12k <= "00";
+                    state_next <= C00;
+                end if;
+            when C01 =>
+                if (i_data = '1') then
+                    o_p12k <= "00";
+                    state_next <= C10;
+                elsif (i_data = '0') then
+                    o_p12k <= "11";
+                    state_next <= C00;
+                end if;
+            when C10 =>
+                if (i_data = '1') then
+                    o_p12k <= "10";
+                    state_next <= C11;
+                elsif (i_data = '0') then
+                    o_p12k <= "01";
+                    state_next <= C01;
+                end if;
+            when C11 =>
+                if (i_data = '1') then
+                    o_p12k <= "01";
+                    state_next <= C11;
+                elsif (i_data = '0') then
+                    o_p12k <= "10";
+                    state_next <= C01;
+                end if;
+        end case;
+    end process;
 end BEHAVIORAL;
